005d2ee9e8d530399367acad1e74c2cf4abc8eb3
Bharath Kumar N <42508577+BharathKumarNLP@users.noreply.github.com>
2024-09-08 21:08:03 -0600

    Update attention_module.py

--- neuromod/attention_module.py
+++ neuromod/attention_module.py
@@ -1,1 +1,15 @@
+class AttentionModule:
+    def __init__(self):
+        self.current_focus = None
 
+    def set_attention(self, focus):
+        self.current_focus = focus
+        print(f"Focus set on: {self.current_focus}")
+
+    def switch_attention(self, new_focus):
+        self.current_focus = new_focus
+        print(f"Switched attention to: {self.current_focus}")
+
+    def get_current_focus(self):
+        return self.current_focus
+# This mimics the brain’s ability to switch focus or multitask.
