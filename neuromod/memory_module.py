9f3924c5312a2abfd0c8a06a82e3224da58c17c3
Bharath Kumar N <42508577+BharathKumarNLP@users.noreply.github.com>
2024-09-08 21:06:41 -0600

    Update memory_module.py

--- neuromod/memory_module.py
+++ neuromod/memory_module.py
@@ -1,1 +1,18 @@
+class HippocampalMemory:
+    def __init__(self):
+        self.short_term_memory = []
+        self.long_term_memory = {}
+
+    def store_short_term(self, data):
+        self.short_term_memory.append(data)
+
+    def commit_to_long_term(self, identifier, data):
+        self.long_term_memory[identifier] = data
+
+    def recall(self, identifier):
+        return self.long_term_memory.get(identifier, "Memory not found")
+
+    def clear_memory(self):
+        self.short_term_memory.clear()
+        self.long_term_memory.clear()
 
